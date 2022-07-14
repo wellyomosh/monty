f87f1ae6a95c86ca9519c903c8b704995b021349
wellyomosh <wellyomosh@gmail.com>
2022-07-13 18:13:37 -0700

    My monty commit

--- create_buffer.c
+++ create_buffer.c
@@ -1,116 +1,42 @@
 #include "monty.h"
 
 /**
- * err - Prints appropiate error messages determined by their error code.
- * @error_code: The error codes are the following:
- * (1) ~> The user does not give any file or more than one file to the program.
- * (2) ~> The file provided is not a file that can be opened or read.
- * (3) ~> The file provided contains an invalid instruction.
- * (4) ~> When the program is unable to malloc more memory.
- * (5) ~> When the parameter passed to the instruction "push" is not an int.
- * (6) ~> When the stack it empty for pint.
- * (7) ~> When the stack it empty for pop.
- * (8) ~> When stack is too short for operation.
- */
-void err(int error_code, ...)
+* create_buff - creates the buffer to parse through files
+* @file_name: name of the file
+*/
+void create_buff(char *file_name)
 {
-	va_list ag;
-	char *op;
-	int l_num;
+	char *str = NULL;
+	char *command = NULL;
+	unsigned int line = 1;
+	size_t size = 0;
+	stack_t *stack = NULL;
+	FILE *file;
 
-	va_start(ag, error_code);
-	switch (error_code)
+	file = fopen(file_name, "r");
+	if (!file)
 	{
-		case 1:
-			printf("USAGE: monty file\n");
-			break;
-		case 2:
-			printf("Error: Can't open file %s\n",
-				va_arg(ag, char *));
-			break;
-		case 3:
-			l_num = va_arg(ag, int);
-			op = va_arg(ag, char *);
-			printf("L%d: unknown instruction %s\n", l_num, op);
-			break;
-		case 4:
-			printf("Error: malloc failed\n");
-			break;
-		case 5:
-			printf("L%d: usage: push integer\n", va_arg(ag, int));
-			break;
-		default:
-			break;
+		printf("Error: Can't open file %s\n", file_name);
+		exit(EXIT_FAILURE);
 	}
-	free_nodes();
-	exit(EXIT_FAILURE);
-}
-
-/**
- * more_err - handles errors.
- * @error_code: The error codes are the following:
- * (6) ~> When the stack it empty for pint.
- * (7) ~> When the stack it empty for pop.
- * (8) ~> When stack is too short for operation.
- * (9) ~> Division by zero.
- */
-void more_err(int error_code, ...)
-{
-	va_list ag;
-	char *op;
-	int l_num;
-
-	va_start(ag, error_code);
-	switch (error_code)
+	argument_container.file = file;
+	while (getline(&str, &size, file) != -1)
 	{
-		case 6:
-			printf("L%d: can't pint, stack empty\n",
-				va_arg(ag, int));
-			break;
-		case 7:
-			printf("L%d: can't pop an empty stack\n",
-				va_arg(ag, int));
-			break;
-		case 8:
-			l_num = va_arg(ag, unsigned int);
-			op = va_arg(ag, char *);
-			printf("L%d: can't %s, stack too short\n", l_num, op);
-			break;
-		case 9:
-			printf("L%d: division by zero\n",
-				va_arg(ag, unsigned int));
-			break;
-		default:
-			break;
+		argument_container.input = str;
+		if (*str == '\n')
+		{
+			line++;
+			continue;
+		}
+		command = strtok(str, " \t\n");
+		if (!command)
+		{
+			line++;
+			continue;
+		}
+		argument_container.arguments = strtok(NULL, " \t\n");
+		get_opcode(&stack, line, command);
+		line++;
 	}
-	free_nodes();
-	exit(EXIT_FAILURE);
-}
-
-/**
- * string_err - handles errors.
- * @error_code: The error codes are the following:
- * (10) ~> The number inside a node is outside ASCII bounds.
- * (11) ~> The stack is empty.
- */
-void string_err(int error_code, ...)
-{
-	va_list ag;
-	int l_num;
-
-	va_start(ag, error_code);
-	l_num = va_arg(ag, int);
-	switch (error_code)
-	{
-		case 10:
-			printf("L%d: can't pchar, value out of range\n", l_num);
-			break;
-		case 11:
-			printf("L%d: can't pchar, stack empty\n", l_num);
-			break;
-		default:
-			break;
-	}
-	free_nodes();
-	exit(EXIT_FAILURE);
+	free_stack(&stack);
 }
