d763c84b0e4768d935bb0792cf9d95b8b8180d04
wellyomosh <wellyomosh@gmail.com>
2022-07-13 18:06:33 -0700

    My monty commit

--- create_buffer.c
+++ create_buffer.c
@@ -1,76 +1,116 @@
-# include "monty.h"
+#include "monty.h"
 
 /**
- * add_node_lifo - adds a new node at the beginning of a stack_t list
- * @stack: head of stack (linked list)
- * @new_node: the node to add
- *
- * Return: void
+ * err - Prints appropiate error messages determined by their error code.
+ * @error_code: The error codes are the following:
+ * (1) ~> The user does not give any file or more than one file to the program.
+ * (2) ~> The file provided is not a file that can be opened or read.
+ * (3) ~> The file provided contains an invalid instruction.
+ * (4) ~> When the program is unable to malloc more memory.
+ * (5) ~> When the parameter passed to the instruction "push" is not an int.
+ * (6) ~> When the stack it empty for pint.
+ * (7) ~> When the stack it empty for pop.
+ * (8) ~> When stack is too short for operation.
  */
-void add_node_lifo(stack_t **stack, stack_t *new_node)
+void err(int error_code, ...)
 {
-	new_node->prev = NULL;
-	new_node->next = *stack;
-	if (*stack)
-		(*stack)->prev = new_node;
-	*stack = new_node;
+	va_list ag;
+	char *op;
+	int l_num;
+
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+		case 1:
+			printf("USAGE: monty file\n");
+			break;
+		case 2:
+			printf("Error: Can't open file %s\n",
+				va_arg(ag, char *));
+			break;
+		case 3:
+			l_num = va_arg(ag, int);
+			op = va_arg(ag, char *);
+			printf("L%d: unknown instruction %s\n", l_num, op);
+			break;
+		case 4:
+			printf("Error: malloc failed\n");
+			break;
+		case 5:
+			printf("L%d: usage: push integer\n", va_arg(ag, int));
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
 }
 
 /**
- * add_node_fifo - adds a new node at the end of a stack_t list
- * @stack: head of stack (linked list)
- * @new_node: the node to add
- *
- * Return: void
+ * more_err - handles errors.
+ * @error_code: The error codes are the following:
+ * (6) ~> When the stack it empty for pint.
+ * (7) ~> When the stack it empty for pop.
+ * (8) ~> When stack is too short for operation.
+ * (9) ~> Division by zero.
  */
-void add_node_fifo(stack_t **stack, stack_t *new_node)
+void more_err(int error_code, ...)
 {
-	stack_t *copy = *stack;
+	va_list ag;
+	char *op;
+	int l_num;
 
-	new_node->next = NULL;
-	if (!*stack)
+	va_start(ag, error_code);
+	switch (error_code)
 	{
-		new_node->prev = NULL;
-		*stack = new_node;
+		case 6:
+			printf("L%d: can't pint, stack empty\n",
+				va_arg(ag, int));
+			break;
+		case 7:
+			printf("L%d: can't pop an empty stack\n",
+				va_arg(ag, int));
+			break;
+		case 8:
+			l_num = va_arg(ag, unsigned int);
+			op = va_arg(ag, char *);
+			printf("L%d: can't %s, stack too short\n", l_num, op);
+			break;
+		case 9:
+			printf("L%d: division by zero\n",
+				va_arg(ag, unsigned int));
+			break;
+		default:
+			break;
 	}
-	else
-	{
-		while (copy->next)
-			copy = copy->next;
-		copy->next = new_node;
-		new_node->prev = copy;
-	}
+	free_nodes();
+	exit(EXIT_FAILURE);
 }
 
 /**
- * _push - adds a new node at the beginning of a stack_t list
- * @stack: head of stack (linked list)
- * @line_number: line number
- *
- * Return: void
+ * string_err - handles errors.
+ * @error_code: The error codes are the following:
+ * (10) ~> The number inside a node is outside ASCII bounds.
+ * (11) ~> The stack is empty.
  */
-void _push(stack_t **stack, unsigned int line_number)
+void string_err(int error_code, ...)
 {
-	stack_t *new_node = NULL;
-	char *num;
-	(void)line_number;
+	va_list ag;
+	int l_num;
 
-	if (inventory->input[1] == NULL)
-		handle_errors(ERROR_PUSH);
-	else
-		num = inventory->input[1];
-	if (are_digits(num) == TRUE)
+	va_start(ag, error_code);
+	l_num = va_arg(ag, int);
+	switch (error_code)
 	{
-		new_node = malloc(sizeof(stack_t));
-		if (new_node == NULL)
-			handle_errors(ERROR_MALLOC);
+		case 10:
+			printf("L%d: can't pchar, value out of range\n", l_num);
+			break;
+		case 11:
+			printf("L%d: can't pchar, stack empty\n", l_num);
+			break;
+		default:
+			break;
 	}
-	else
-		handle_errors(ERROR_PUSH);
-
-	new_node->n = atoi(num);
-	if (inventory->order == LIFO)
-		add_node_lifo(stack, new_node);
-	else
-		add_node_fifo(stack, new_node);
+	free_nodes();
+	exit(EXIT_FAILURE);
 }
