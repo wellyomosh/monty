de6e3dffc008c037fe8913e27386359221368410
wellyomosh <wellyomosh@gmail.com>
2022-07-13 17:53:44 -0700

    My monty commit

--- create_buffer.c
+++ create_buffer.c
@@ -1,135 +1,122 @@
 #include "monty.h"
 
 /**
- * read_f - open and reads file
- * @file_name: file name
- * Return: file file content
+ * op_push - pushes an element to the stack
+ * @stack: pointer to the pointer to the first element in stack
+ * @line_number: line number of the instruction
  */
-
-char *read_f(char *file_name)
+void op_push(stack_t **stack, unsigned int line_number)
 {
-	FILE *fp;
-	char *buffer;
-	long int buf_size;
-	struct stat st;
+	int num;
+	stack_t *new;
 
-	if (stat(file_name, &st) == 0)
-		buf_size = st.st_size;
-
-	buffer = malloc(sizeof(char) * buf_size);
-	if (!buffer)
+	if (is_valid_int(op.arg) == 0)
 	{
-		perror("Error: malloc failed");
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		free_op();
+		exit(EXIT_FAILURE);
+	}
+	num = atoi(op.arg);
+
+	/* add num to beginning of stack */
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failedr\n");
+		free_op();
 		exit(EXIT_FAILURE);
 	}
 
-	fp = fopen(file_name, "r");
-	if (!fp)
+	new->n = num;
+	new->prev = NULL;
+	new->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new;
+
+	*stack = new;
+}
+
+
+/**
+ * op_pall - prints all the values on the stack, from the top
+ * @stack: pointer to the pointer to the first element in stack
+ * @line_number: line number of the instruction
+ */
+void op_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *trav = *stack;
+
+	while (trav)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", file_name);
+		printf("%d\n", trav->n);
+		trav = trav->next;
+	}
+
+	(void)line_number;
+}
+
+
+/**
+ * op_pint - prints the value at the top of the stack
+ * @stack: pointer to the pointer to the first element in stack
+ * @line_number: line number of the instruction
+ */
+void op_pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		free_op();
 		exit(EXIT_FAILURE);
 	}
-	fread(buffer, sizeof(char), buf_size, fp);
 
-	fclose(fp);
-	return (buffer);
+	printf("%d\n", (*stack)->n);
 }
 
+
 /**
- * parse_f - parse buffer
- * @file_name:file name
- * Return: file file content
+ * op_pop - removes the top element of the stack
+ * @stack: pointer to the pointer to the first element in stack
+ * @line_number: line number of the instruction
  */
-
-char **parse_f(char *file_name)
+void op_pop(stack_t **stack, unsigned int line_number)
 {
-	char **commands;
-	long int buf_size;
-	char *buffer;
-	struct stat st;
-	int i = 0;
+	stack_t *tmp;
 
-	if (stat(file_name, &st) == 0)
-		buf_size = st.st_size;
-
-	buffer = malloc(sizeof(char) * buf_size);
-	commands = malloc(sizeof(char *) * buf_size);
-	buffer = read_f(file_name);
-
-	commands[i] = strtok(buffer, " \n");
-	while (commands[i] != NULL)
+	if (*stack == NULL)
 	{
-		i++;
-		commands[i] = strtok(NULL, " \n");
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		free_op();
+		exit(EXIT_FAILURE);
 	}
-	free(buffer);
-	return (commands);
+
+	tmp = *stack;
+	*stack = (*stack)->next;
+	free(tmp);
+
+	if (*stack)
+		(*stack)->prev = NULL;
 }
 
-/**
- * add_to_stack - change stack
- * @file_name: file name
- * Return: 1 at success
- */
-
-int add_to_stack(char *file_name)
-{
-	stack_t **head;
-	struct stat st;
-	long int buf_size;
-	char **commands;
-	unsigned int i = 0;
-
-	if (stat(file_name, &st) == 0)
-		buf_size = st.st_size;
-
-	head = malloc(sizeof(char) * buf_size);
-	commands = malloc(sizeof(char *) * buf_size);
-	commands = parse_f(file_name);
-	while (commands[i] != NULL)
-	{
-		if (strcmp(commands[i], "push") == 0)
-		{
-			if (atoi(commands[i + 1]) != 0)
-				push(head, atoi(commands[i + 1]));
-			else
-			{
-				fprintf(stderr, "L%d: usage: push integer\n", i);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			get_op_func(commands[i], head, i);
-		}
-		i++;
-	}
-	free_dlistint(*head);
-	free(commands);
-	return (1);
-}
 
 /**
- * get_op_func - get functions
- * @s: command
- * @head: top of stack
- * @n: line_number
+ * op_swap - swaps the top two elements of the stack
+ * @stack: pointer to the pointer to the first element in stack
+ * @line_number: line number of the instruction
  */
-
-void get_op_func(char *s, stack_t **head, unsigned int n)
+void op_swap(stack_t **stack, unsigned int line_number)
 {
-	instruction_t ops[] = {
-		{"pall", pall},
-		{"pint", _pint},
-		{NULL, NULL}
-	};
-	int i;
+	int tmp;
 
-	i = 0;
-	while (ops[i].opcode != NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		if (strcmp(ops[i].opcode, s) == 0)
-			ops[i].f(head, n);
-		i++;
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		free_op();
+		exit(EXIT_FAILURE);
 	}
+
+	tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
 }
